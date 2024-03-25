2d1d208351d1862953412551cd6dfaca0127fbda
raulzovisk <raulmodestosousa@gmail.com>
2024-03-24 22:59:45 -0300

    24/03

--- resources/views/first-login.blade.php
+++ resources/views/first-login.blade.php
@@ -6,37 +6,35 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <style>
     body {
-        background: rgb(13, 131, 154);
-        background: linear-gradient(107deg, rgba(13, 131, 154, 1) 5%, rgba(95, 162, 218, 1) 52%, rgba(187, 164, 245, 1) 94%);
+        background: rgb(224, 224, 224);
         font-family: 'Montserrat', sans-serif;
     }
-
 
     .opcoes-container {
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100vh;
-        gap: 20px;
+        height: 500px;
+        gap: 60px;
     }
 
     .opcao {
         background-color: #f0f0f0;
         border: 1px solid #ccc;
-        border-radius: 5px;
+        border-radius: 10px;
         padding: 20px;
         width: 270px;
-        height: 80%;
+        height: 70%;
         text-align: center;
         transition: transform 0.3s ease;
         display: flex;
         align-items: center;
         justify-content: center;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     }
 
     .opcao:hover {
-        transform: scale(1.19);
+        transform: scale(1.06);
         box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
     }
 
@@ -48,9 +46,13 @@
 
     }
 </style>
-
+<x-app-layout>
 <body>
-
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Cadastro') }}
+        </h2>
+    </x-slot>
     <div class="opcoes-container">
         <div class="opcao">
             <a href="{{ route('Aluno.create') }}">Cadastro de Aluno</a>
@@ -61,3 +63,4 @@
     </div>
 
 </body>
+</x-app-layout>
